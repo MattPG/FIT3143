7125979e57808c34404f86ba546ff22ba53b8b62
MattPG <mattpgreenwood@gmail.com>
2014-03-18 16:04:35 +1100

    Initial Commit.

---
+++ Files/Lab2/exercise1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : exercise1.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+}
