55a820e6f31bba6104a7edf5d478800c092163b1
mpgre4 <mpgre4@localhost>
2014-04-28 14:38:21 +1000

    pretty much finished part 1 of lab4

--- Files/Lab4/HelloWorldMPI.c
+++ Files/Lab4/HelloWorldMPI.c
@@ -15,13 +15,12 @@
 int main(int argc, char* argv[]){
 	int  myRank; /* rank of process */
 	int  numProc;       /* number of processes */
-	int source;   /* rank of sender */
-	int dest;     /* rank of receiver */
-	int tag=0;    /* tag for messages */
-	int NUM_ITEMS = 10000; // number of items to process
-	char message[100];        /* storage for message */
 	MPI_Status status ;   /* return status for receive */
 	
+	int NUM_ITEMS = 10000; // number of items to process
+	int MASTER = 0;
+	int TARGET_FOUND_TAG = 0;
+
 	/* start up MPI */
 	MPI_Init(&argc, &argv);
 
@@ -31,42 +30,68 @@ int main(int argc, char* argv[]){
 	/* find out process rank */
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 	
-	if (myRank !=0){ // Slaves
-		// find the number of items for this process
-		int numItems = NUM_ITEMS/(numProc-1);
-		// get the analysis domain of this process
-		int maxElement = (myRank)*(numItems);
-		int minElement = (myRank-1)*(numItems);
-		// generate the table
-		int table[numItems];
-		// fill the table
-		int i;
-		for(i = minElement; i < maxElement; i++){
-			table[i] = i;
-		}
+	if (myRank == MASTER){ // Master
+		while(1){
 
-		/* create message */
-		sprintf(message, "Process %d! Computing between [%i,%i).", myRank,minElement,maxElement);
-		dest = 0;
-		/* use strlen+1 so that '\0' gets transmitted */
-		MPI_Send(message, strlen(message)+1, MPI_CHAR,
-		   dest, tag, MPI_COMM_WORLD);
-	}
-	else{ // Master
-		printf("Please enter a number below %i.", NUM_ITEMS);
+			int toFind;
+			// Get value to look for from user
+			printf("Please enter an integer between 0 and %i\n", NUM_ITEMS);
+			scanf("%i", &toFind);
 
-		// Search for incoming messages from each process
-		for (source = 1; source < numProc; source++) {
-			MPI_Recv(message, 100, MPI_CHAR, source, tag,
-			      MPI_COMM_WORLD, &status);
-			printf("%s\n",message);
+			// Notify slaves of value
+			MPI_Bcast(&toFind, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+			if(toFind < 0)
+				break;
+
+			int slaveID;
+			MPI_Recv(&slaveID, 1, MPI_INT, MPI_ANY_SOURCE, TARGET_FOUND_TAG, MPI_COMM_WORLD, &status);
+
+			printf("The number was found by task#%i!!!!\n", slaveID);
 		}
 	}
+	else{ // Slaves
+
+			// find the number of items for this process
+			int numItems = NUM_ITEMS/(numProc-1);
+			// get the analysis domain of this process
+			int maxElement = (myRank)*(numItems);
+			int minElement = (myRank-1)*(numItems);
 
 
+			// generate the table
+			int* table = (int*) malloc(numItems * sizeof(int));
+			// populate the table
+			int i;
+			int count = minElement;
+			for(i = 0; i < numItems; i++){
+				table[i] = count++;
+			}
+
+			int toFind, toSend;
+
+		while(1){
+
+			// Get value to look for from master
+			MPI_Bcast(&toFind, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+			if(toFind < 0)
+				break;
+
+			toSend = 0;
+			// parse the array for the value to find
+
+			if(toFind < maxElement && toFind >= minElement)
+				// Send success tag
+				toSend = 1;
+
+
+			MPI_Gather();
+		}
+	}
+	
 	/* shut down MPI */
-	MPI_Finalize(); 
-	
-	
+	MPI_Finalize();
+
 	return EXIT_SUCCESS;
 }
